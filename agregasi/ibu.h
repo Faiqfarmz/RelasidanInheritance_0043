fd57b28fa69ab381a04b7ac9e2cec0edbd94b511
Faiqfarmz <faiqfarmz@gmail.com>
2025-05-26 11:55:35 +0700

    class ibu

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -2,3 +2,19 @@
 #ifndef IBU_H
 #define IBU_H
 
+class ibu {
+    public :
+        string nama;
+        vector<anak*> daftar_anak;
+
+        ibu (string pNama) :nama(pNama) {
+            cout << "ibu \""<< nama << "\" ada\n";
+        }
+        ~ibu(){
+            cout << "ibu \"" << nama << "\" tidak ada\n";
+        }
+
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
+
