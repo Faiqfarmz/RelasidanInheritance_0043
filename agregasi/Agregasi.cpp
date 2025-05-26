d58dea3367ddb0c02195ac55c2fb6337173f211a
Faiqfarmz <faiqfarmz@gmail.com>
2025-05-26 11:51:21 +0700

    include agregasi

--- agregasi/Agregasi.cpp
+++ agregasi/Agregasi.cpp
@@ -3,29 +3,3 @@
 #include <vector>
 using namespace std;
 
-#include "anak.h"
-#include "ibu.h"
-
-int main() {
-    ibu* varibu1 = new ibu("dini");
-    ibu* varibu2 = new ibu("novi");
-    anak* varAnak1 = new anak("tono");
-    anak* varAnak2 = new anak("rini");
-    anak* varAnak3 = new anak("dewi");
-
-    varibu1->tambahAnak(varAnak1);
-    varibu1->tambahAnak(varAnak2);
-    varibu2->tambahAnak(varAnak3);
-    varibu2->tambahAnak(varAnak1);
-
-    varibu1->cetakAnak();
-    varibu2->cetakAnak();
-
-    delete varibu1;
-    delete varibu2;
-    delete varAnak1;
-    delete varAnak2;
-    delete varAnak3;
-
-    return 0;
-}
--- asotiation/asosiation.cpp
+++ asotiation/asosiation.cpp
@@ -6,5 +6,31 @@ using namespace std;
 class dokter;
 class pasien { //asosiasi
     public :
-        string nama;    
-}
+        string nama;
+        vector<dokter*> daftar_dokter;
+        pasien(string pNama) :nama(pNama) {
+            cout << "pasien \"" << nama << "\" ada\n";
+        }
+        ~pasien() {
+            cout << "pasien \"" << nama << "\" tidak ada\n";
+        }
+        void tambahDokter(dokter*);
+        void cetakDokter();
+};
+
+class dokter {
+    public :
+        string nama;
+        vector<pasien*> daftar_pasien;
+
+        dokter(string pNama) :nama(pNama) {
+            cout << "Dokter \"" << nama << "\" ada\n";
+        }
+    ~dokter(){
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};
+
