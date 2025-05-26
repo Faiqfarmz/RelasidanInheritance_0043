4ea3ab1089dfd982a57e109e29535cfee4d849ef
Faiqfarmz <faiqfarmz@gmail.com>
2025-05-26 11:54:14 +0700

    class anak

--- agregasi/anak.h
+++ agregasi/anak.h
@@ -1,3 +1,14 @@
 #ifndef ANAK_H
 #define ANAK_H
 
+class anak {
+    public :
+        string nama;
+        anak (string pNama) :nama(pNama) {
+            cout << "Anak \"" << "\" ada \n";
+        }
+        ~anak() {
+            cout << "Anal \"" << nama << "\" tidak ad\n"; 
+        }
+};
+#endif
